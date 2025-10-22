ecb23841669a0796195ea88b6ed006998ab1221a
Zexsch <awildzexsch@gmail.com>
2025-10-22 21:44:37 +0200

    Fixed incorrect bot initalisation

--- src/Bot/app.py
+++ src/Bot/app.py
@@ -14,7 +14,7 @@ class App(commands.Bot):
     def __init__(self):
         intents = discord.Intents.default()
         intents.message_content = True
-        self.bot = commands.Bot(command_prefix=PREFIX, intents=intents, help_command=None, owner_id=OWNER_ID)
+        self.bot = super().__init__(command_prefix=PREFIX, intents=intents, help_command=None, owner_id=OWNER_ID)
         
         self.logger = SingletonLogger().get_logger()
         self.database = DatabaseHandler()
@@ -31,14 +31,14 @@ class App(commands.Bot):
         from Bot.Cogs.deepseek_cog import DeepseekCog
         
         self.logger.info("Initialising cogs.")
-        await self.bot.add_cog(ListenerCog(bot=self.bot, database=self.database, logger=self.logger))
-        await self.bot.add_cog(GwensubCog(bot=self.bot, database=self.database, logger=self.logger))
-        await self.bot.add_cog(OwnerCog(bot=self.bot, database=self.database, logger=self.logger))
-        await self.bot.add_cog(WinrateCog(bot=self.bot, winrate_fetcher=self.winrate_fetcher, logger=self.logger))
-        await self.bot.add_cog(DMCog(bot=self.bot, winrate_fetcher=self.winrate_fetcher))
-        await self.bot.add_cog(CommandsCog(bot=self.bot))
-        await self.bot.add_cog(LeaderboardCog(bot=self.bot, database=self.database))
-        await self.bot.add_cog(DeepseekCog(bot=self.bot, database=self.database, logger=self.logger))
+        await self.add_cog(ListenerCog(bot=self, database=self.database, logger=self.logger))
+        await self.add_cog(GwensubCog(bot=self, database=self.database, logger=self.logger))
+        await self.add_cog(OwnerCog(bot=self, database=self.database, logger=self.logger))
+        await self.add_cog(WinrateCog(bot=self, winrate_fetcher=self.winrate_fetcher, logger=self.logger))
+        await self.add_cog(DMCog(bot=self, winrate_fetcher=self.winrate_fetcher))
+        await self.add_cog(CommandsCog(bot=self))
+        await self.add_cog(LeaderboardCog(bot=self, database=self.database))
+        await self.add_cog(DeepseekCog(bot=self, database=self.database, logger=self.logger))
         self.logger.info("Finished initialising cogs.")
             
         
